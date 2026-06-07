b9b3dc08d8510aeed4c85f3a4139735821510451
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-06-07 10:05:22 +0200

    Repair trip screen file

--- web/js/screens/tripScreen.js
+++ web/js/screens/tripScreen.js
@@ -1,10 +1,12 @@
-import {state,setState,trips} from '../state.js';
-import {el,button} from '../utils/dom.js';
-import {bottomNav} from '../components/bottomNav.js';
-import {sectionCard} from '../components/sectionCard.js';
-import {scheduleTimeline} from '../components/scheduleTimeline.js';
-import {detailsGrid} from '../components/detailsGrid.js';
-import {linksGrid} from '../components/linksGrid.js';
-import {statusChips} from '../components/statusChips.js';
-import {imageBox,findLocationImage,findEventImage} from '../components/imageBox.js';
-import {iconButton
+import { state, setState, trips } from '../state.js';
+import { el, button } from '../utils/dom.js';
+import { bottomNav } from '../components/bottomNav.js';
+import { sectionCard } from '../components/sectionCard.js';
+import { scheduleTimeline } from '../components/scheduleTimeline.js';
+import { detailsGrid } from '../components/detailsGrid.js';
+import { linksGrid } from '../components/linksGrid.js';
+import { statusChips } from '../components/statusChips.js';
+import { imageBox, findLocationImage, findEventImage } from '../components/imageBox.js';
+import { iconButton } from '../components/iconButton.js';
+import { tripEntries, tripTitle, tripRange, candidateDays } from '../features/tripFeature.js';
+import { addDayToTrip, removeDay
