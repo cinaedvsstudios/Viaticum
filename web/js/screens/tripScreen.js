4ebe28f334e1f374405e0e926c19bbad8dc03756
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-06-07 11:39:53 +0200

    Restore trip screen

--- web/js/screens/tripScreen.js
+++ web/js/screens/tripScreen.js
@@ -1,9 +1,10 @@
-import{state,setState,trips}from'../state.js';
-import{el,button}from'../utils/dom.js';
-import{bottomNav}from'../components/bottomNav.js';
-import{sectionCard}from'../components/sectionCard.js';
-import{scheduleTimeline}from'../components/scheduleTimeline.js';
-import{detailsGrid}from'../components/detailsGrid.js';
-import{linksGrid}from'../components/linksGrid.js';
-import{statusChips}from'../components/statusChips.js';
-import{imageBox,
+import { state, setState, trips } from '../state.js';
+import { el, button } from '../utils/dom.js';
+import { bottomNav } from '../components/bottomNav.js';
+import { tripEntries, tripTitle, tripRange } from '../features/tripFeature.js';
+import { pushRoute, goMain } from '../router/history.js';
+import { sheetUrl } from '../config.js';
+import { syncAll } from '../services/syncService.js';
+
+export function renderTripScreen() {
+  const
