9bc59cd447122d0d2e825c64a8590f3f91f523ab
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-06-07 10:59:46 +0200

    Restore trip screen file

--- web/js/screens/tripScreen.js
+++ web/js/screens/tripScreen.js
@@ -1,12 +1,11 @@
-import { state,setState,trips } from '../state.js';
-import { el,button } from '../utils/dom.js';
+import { state, setState, trips } from '../state.js';
+import { el, button } from '../utils/dom.js';
 import { bottomNav } from '../components/bottomNav.js';
 import { sectionCard } from '../components/sectionCard.js';
 import { scheduleTimeline } from '../components/scheduleTimeline.js';
 import { detailsGrid } from '../components/detailsGrid.js';
 import { linksGrid } from '../components/linksGrid.js';
 import { statusChips } from '../components/statusChips.js';
-import { imageBox,findLocationImage,findEventImage } from '../components/imageBox.js';
+import { imageBox, findLocationImage, findEventImage } from '../components/imageBox.js';
 import { iconButton } from '../components/iconButton.js';
-import { tripEntries,tripTitle,tripRange,candidateDays } from '../features/tripFeature.js';
-import { addDayToTrip,remove
+import { tripEntries, tripTitle, trip
