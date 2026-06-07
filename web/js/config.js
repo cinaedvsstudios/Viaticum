dfe4b1cbebc7a690f64967da0913e0f5de2045ee
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-06-07 07:32:14 +0200

    Add Google OAuth client ID

--- web/js/config.js
+++ web/js/config.js
@@ -4,7 +4,7 @@ export const config = {
   mainRange: 'sheet1!A2:I',
   refRange: 'ref!A2:Q',
   rawImageBaseUrl: 'https://raw.githubusercontent.com/cinaedvsstudios/Viaticum/main/',
-  googleClientId: 'PASTE_GOOGLE_OAUTH_CLIENT_ID_HERE.apps.googleusercontent.com',
+  googleClientId: '1095336877076-4ni688klccnish4j4pl28nm40379lnvc.apps.googleusercontent.com',
   scopes: 'https://www.googleapis.com/auth/spreadsheets'
 };
 export const sheetUrl = () => `https://docs.google.com/spreadsheets/d/${config.spreadsheetId}`;
