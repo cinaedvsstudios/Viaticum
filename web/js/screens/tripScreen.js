b827813900cc8929ef5ac1c12ce7f3bcbf5117c3
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-06-07 11:47:34 +0200

    Repair trip screen

--- web/js/screens/tripScreen.js
+++ web/js/screens/tripScreen.js
@@ -1,10 +1,19 @@
-import { state, setState, trips } from '../state.js';
-import { el, button } from '../utils/dom.js';
+import { state,setState,trips } from '../state.js';
+import { el,button } from '../utils/dom.js';
 import { bottomNav } from '../components/bottomNav.js';
-import { tripEntries, tripTitle, tripRange } from '../features/tripFeature.js';
-import { pushRoute, goMain } from '../router/history.js';
+import { tripEntries,tripTitle,tripRange } from '../features/tripFeature.js';
+import { pushRoute,goMain } from '../router/history.js';
 import { sheetUrl } from '../config.js';
 import { syncAll } from '../services/syncService.js';
 
-export function renderTripScreen() {
-  const
+export function renderTripScreen(){
+  const list=trips();
+  const chosen=state.selectedTripName||list[0]||'';
+  if(chosen!==state.selectedTripName)setState({selectedTripName:chosen});
+  return el('main',{class:'screen trip-screen'},
+    el('div',{class:'trip-desktop-grid'},tripList(list,chosen),tripDetail(chosen)),
+    bottomNav(state.refData,{homeLabel:'Home',day:goMain,trip:()=>{},sheet:()=>window.open(sheetUrl(),'_blank','noopener'),sync:syncAll,more:()=>setState({modal:'more'})},'trip')
+  );
+}
+
+function tripList(list
